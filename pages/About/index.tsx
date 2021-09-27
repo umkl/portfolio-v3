5af2c7b55546a538ab5b620049d280a72093a390
ungarmichael <ungarmichael.mail@gmail.com>
2021-09-27 18:28:22 +0200

    FEAT: about_image

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import f from "./../../styles/fonts.module.scss";
 import c from "./../../styles/components.module.scss";
 import l from "./about.module.scss";
+import Image from "next/image";
+import setup from "./../../public/images/setup.jpg";
 
 // import aboutStyles from "./about.module.scss";
 // import utilStyles from "./../../styles/utils.module.scss";
@@ -11,7 +13,7 @@ function About() {
   return (
     <>
       <Head>
-        <title>About - Octagon</title>
+        <title>ungarmichael - portfolio</title>
       </Head>
 
       <section className={c["Section"]}>
@@ -19,28 +21,54 @@ function About() {
           <span className={f["Section-Code-1"]}>01.</span>
           <span className={f["Section-Heading-1"]}>About</span>
         </h2>
-        <p className={`${l["Text-Container"]} ${f["About-Text-1"]}`}>
-          Hello! My name is Michael and I enjoy creating things that live on the
-          internet. My interest in programming started in 2018 when I learned
-          about the possiblities of programming over soloLearn. My
-          <br />
-          <br />
-          Later I
-          <br />
-          <br />
-          Fast-forward to today, and I've had the privilege of working at an
-          advertising agency, a start-up, a huge corporation, and a student-led
-          design studio. My main focus these days is building accessible,
-          inclusive products and digital experiences at Upstatement for a
-          variety of clients.
-          <br />
-          I also recently launched a course that covers everything you need to
-          build a web app with the Spotify API using Node & React.
-          <br />
-          <br />
-          <br />
-          Here are a few technologies I've been working with recently:
-        </p>
+        <div className={l["Content-Wrapper"]}>
+          <p className={`${l["Text-Container"]} ${f["About-Text-1"]}`}>
+            Hello!, I am Michael and I really enjoy creating things. My interest
+            in tech in general started with lego mindstorms and
+            circuit-soldering kits back when I was about 8 years old. After
+            watching countless youtube-tutorials my parents allowed me to build
+            my own computer after proving them I was capable of doing so.
+            Eventually I got into coding when trying to create minecraft mods
+            and servers. In my first year at the HTL Grieskirchen for Computer
+            Science I was able to learn about all of the possibilites with code.
+            <br />
+            <br />
+            After completing many youtube tutorials and online courses I
+            continuesly found myself at creating web applications with ReactJS.
+            I also learned a lot about project management because I wanted to
+            have my creations as structured and organised as possible even
+            though it sometimes cost me more time than actually coding it.
+            <br />
+            <br />
+            Right now I design, manage and develop projects for various clients
+            and as already meantioned I am attending highschool. Eventually I
+            would like to work in the tech-industry creating awesome projects
+            with awesome people!
+            <br />
+            <br />
+            Besides that I also love to interact with coding-communities and
+            implement my own ideas.
+            <br />
+            <br />
+            Here are a few technologies I am currently working with:
+          </p>
+          <div className={l["Image-Wrapper"]}>
+            <div className={l["Image-Filter"]}></div>
+            <div className={l["Image-Mask"]}>
+              <div className={l["Image-Container"]}>
+                <Image
+                  className={l["Image"]}
+                  layout="responsive"
+                  height={500}
+                  width={900}
+                  src={setup}
+                  alt="Picture of the author"
+                />
+              </div>
+            </div>
+            <div className={l["Image-Frame"]}>miksl image</div>
+          </div>
+        </div>
       </section>
     </>
   );
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,11 +1,24 @@
 import f from "./../../styles/fonts.module.scss";
 import l from "./home.module.scss";
 import c from "./../../styles/components.module.scss";
+import { gsap, TweenMax, Power3 } from "gsap";
+import { useEffect, useRef } from "react";
 
 const Home = (): JSX.Element => {
+  const homeRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(homeRef.current, {
+      opacity: 1,
+      top: "50%",
+      duration: 0.8,
+      ease: Power3.easeOut,
+    });
+  }, []);
+
   return (
     <section className={l["Section"]}>
-      <div className={l["Container"]}>
+      <div ref={homeRef} className={l["Container"]}>
         <h1>
           <p className={f["Home-Code-1"]}>Hi, my name is</p>
           <p className={f["Home-Heading-1"]}>Michael Ungar.</p>
