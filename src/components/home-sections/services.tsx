82b9b15448bd8e20bdf547815e104179bc3b3dfe
Michael Ungar <michael.ungar@paso-solutions.com>
2022-08-15 21:52:57 +0200

    simple padding adjusting

--- src/components/home-sections/services.tsx
+++ src/components/home-sections/services.tsx
@@ -8,25 +8,30 @@ const Services = () => {
       <h1 className={servicesStyle.heading}>
         <span className={servicesStyle.code}>03.</span> <span> Services</span>
       </h1>
-      <div id={servicesStyle.gig}>
-        <h2 className={servicesStyle.heading2}>
-          <span className={servicesStyle.code}>03.1.</span>{" "}
-          <span>Webdevelopment</span>
-        </h2>
-        <div id={servicesStyle.split}>
-          <div id={servicesStyle.text}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-              laudantium adipisci non, id voluptatem ex quas, recusandae
-              doloremque distinctio culpa totam optio? Mollitia, eaque obcaecati
-              quasi molestiae repellendus et ut.
-            </p>
-            <button id={servicesStyle.reachbtn}>reach out to me</button>
-            <button id={servicesStyle.fiverrbtn}> fiverr gig</button>
-            <button id={servicesStyle.upworkbtn}>upwork</button>
-          </div>
-          <div id={servicesStyle.picture}>picture</div>
+      <Service/>
+      <Service/>
+      <Service/>
+    </div>
+  );
+};
+
+const Service = () => {
+  return (
+    <div id={servicesStyle.gig}>
+      <h2 className={servicesStyle.heading2}>
+        <span className={servicesStyle.code}>03.1.</span>{" "}
+        <span>Webdevelopment</span>
+      </h2>
+      <div id={servicesStyle.split}>
+        <div id={servicesStyle.text}>
+          <p>
+            I am offering the service to build your website. I can build your
+            website from scratch or update your existing website.
+          </p>
+          <button id={servicesStyle.reachbtn}>reach out to me</button>
+          <button id={servicesStyle.fiverrbtn}>fiverr gig</button>
         </div>
+        {/* <div id={servicesStyle.picture}>picture</div> */}
       </div>
     </div>
   );
